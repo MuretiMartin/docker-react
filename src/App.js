8e8e1b19986006f7d1defb5bae22ada315359db8
MMmuretiMartin <mmureti.martin@gmail.com>
2023-03-04 01:33:11 +0300

    added my name

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          I was changed
+          My Name is Mureti Martin
         </p>
         <a
           className="App-link"
